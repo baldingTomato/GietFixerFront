486a7178a9547049a30378cfc516c354940e6e17
baldingTomato <tomek.kluba12@gmail.com>
2023-01-09 19:36:48 +0100

    Created repo for front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'll';
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    // if (this.isLoggedIn) {
+    //   const user = this.tokenStorageService.getUser();
+    //   this.roles = user.roles;
+
+    //   this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+    //   this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+    //   this.username = user.username;
+    // }
+  }
+
+  logout(): void {
+    // this.tokenStorageService.signOut();
+    // window.location.reload();
+  }
 }
